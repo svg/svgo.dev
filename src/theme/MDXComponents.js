1eb48c3619ecdfba94df85f26dd853cd281c1982
Seth Falco <seth@falco.fun>
2026-08-14 18:55:47 +0100

    chore(mdx): remove DefaultBadge from MDXComponents
    We don't need this anymore. MDXComponents are for being able to use
    components in markup from svg/svgo. However, we'll handle presets from
    here now through the JS API.

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -1,7 +1,6 @@
 import TabItem from '@theme/TabItem';
 import Tabs from '@theme/Tabs';
 import MDXComponents from '@theme-original/MDXComponents';
-import DefaultBadge from '../components/DefaultBadge';
 import PluginDemo from '../components/PluginDemo';
 import PluginParams from '../components/PluginParams';
 import PluginUsage from '../components/PluginUsage';
@@ -11,7 +10,6 @@ export default {
   ...MDXComponents,
   Tabs,
   TabItem,
-  DefaultBadge,
   PluginDemo,
   PluginParams,
   PluginUsage,
